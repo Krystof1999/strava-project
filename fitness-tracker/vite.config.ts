1a0ce3f039026cb4678a7cc8980fcd7c51177224
Krystof Bumbalek <krystof.bumbalek@saboit.de>
2023-09-22 19:48:41 +0200

    vite.config.ts update

--- fitness-tracker/vite.config.ts
+++ fitness-tracker/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/my-app/", // Set the base URL for deployment
+  plugins: [
+    react(), // Enable React support
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // Define aliases for commonly used directories
+    },
+  },
+  build: {
+    outDir: "dist", // Specify the output directory for the production build
+  },
+});
