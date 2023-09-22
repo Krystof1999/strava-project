c550006faab4a79fb2f304fe56dc37a20a5aee60
Krystof Bumbalek <krystof.bumbalek@saboit.de>
2023-09-22 20:48:12 +0200

    build test update

--- fitness-tracker/src/components/Test.tsx
+++ fitness-tracker/src/components/Test.tsx
@@ -1,5 +1,5 @@
-// import { DateTime } from "luxon";
-// import useStravaActivities from "../hooks/useStravaActivities";
+import { DateTime } from "luxon";
+import useStravaActivities from "../hooks/useStravaActivities";
 
 // ? one week
 // // Calculate Luxon DateTime objects for the start and end of September 18, 2023
@@ -16,30 +16,30 @@
 // console.log(oneDayAgoTimestamp);
 
 // //? one day
-// // Calculate Luxon DateTime objects for the start and end of September 20, 2023
-// const startDate = DateTime.fromISO("2023-09-20T00:00:00Z");
-// const endDate = DateTime.fromISO("2023-09-20T23:59:59Z");
+// Calculate Luxon DateTime objects for the start and end of September 20, 2023
+const startDate = DateTime.fromISO("2023-09-20T00:00:00Z");
+const endDate = DateTime.fromISO("2023-09-20T23:59:59Z");
 
 // Convert Luxon DateTime objects to epoch timestamps in seconds
-// const startDateTimestamp = Math.floor(startDate.toSeconds());
-// const endDateTimestamp = Math.floor(endDate.toSeconds());
+const startDateTimestamp = Math.floor(startDate.toSeconds());
+const endDateTimestamp = Math.floor(endDate.toSeconds());
 
 const Test = () => {
-  //   const {
-  //     data: activities,
-  //     error,
-  //     isLoading,
-  //   } = useStravaActivities(startDateTimestamp, endDateTimestamp);
+  const {
+    data: activities,
+    error,
+    isLoading,
+  } = useStravaActivities(startDateTimestamp, endDateTimestamp);
 
-  //   console.log(activities);
-  //   console.log(error);
-  //   console.log("isLoading", isLoading);
+  console.log(activities);
+  console.log(error);
+  console.log("isLoading", isLoading);
   return (
     <div>
       Test
-      {/* {activities?.map((activity) => (
+      {activities?.map((activity) => (
         <li>{activity.sport_type}</li>
-      ))} */}
+      ))}
     </div>
   );
 };
