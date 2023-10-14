ca7463458a2556eb368c7333771c110fa4927ae8
Krystof Bumbalek <krystof.bumbalek@saboit.de>
2023-10-14 17:28:46 +0200

    map style changed

--- fitness-tracker/src/components/Map.tsx
+++ fitness-tracker/src/components/Map.tsx
@@ -34,9 +34,17 @@ const Map = ({ coordinates, mapPolylines }: Props) => {
       id="map"
       zoomControl={false}
     >
-      <TileLayer
+      {/* <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      /> */}
+      {/* <TileLayer
+        url="https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+        attribution='&copy; <a href="https://www.esri.com/">Esri</a>'
+      /> */}
+      <TileLayer
+        url="https://tile.jawg.io/jawg-terrain/{z}/{x}/{y}.png?access-token=YBi9f27w4DIZfxr4OHJfg1MQSCeQerLL8fh0e9ZuVLBWciU2pFOzXcFzgWVdWk1v"
+        attribution='&copy; <a href="https://www.jawg.io/">Jawg</a>'
       />
 
       <Polyline pathOptions={{ color: "blue" }} positions={mapPolylines} />
