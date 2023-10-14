6654a422cda0463d2374bd984a1a551419f542bd
Krystof Bumbalek <krystof.bumbalek@saboit.de>
2023-10-14 17:36:12 +0200

    default map

--- fitness-tracker/src/components/Map.tsx
+++ fitness-tracker/src/components/Map.tsx
@@ -34,18 +34,25 @@ const Map = ({ coordinates, mapPolylines }: Props) => {
       id="map"
       zoomControl={false}
     >
+      {/* smooth dark */}
       {/* <TileLayer
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>'
+      /> */}
+
+      {/* default */}
+      <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      /> */}
+      />
       {/* <TileLayer
         url="https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
         attribution='&copy; <a href="https://www.esri.com/">Esri</a>'
       /> */}
-      <TileLayer
+      {/* <TileLayer
         url="https://tile.jawg.io/jawg-terrain/{z}/{x}/{y}.png?access-token=YBi9f27w4DIZfxr4OHJfg1MQSCeQerLL8fh0e9ZuVLBWciU2pFOzXcFzgWVdWk1v"
         attribution='&copy; <a href="https://www.jawg.io/">Jawg</a>'
-      />
+      /> */}
 
       <Polyline pathOptions={{ color: "blue" }} positions={mapPolylines} />
       <MapUpdater />
