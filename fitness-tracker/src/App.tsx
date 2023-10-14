3b3cc85721a8fad4d94291912f8f1e29af3d682f
Krystof Bumbalek <krystof.bumbalek@saboit.de>
2023-10-14 17:54:40 +0200

    map test

--- fitness-tracker/src/App.tsx
+++ fitness-tracker/src/App.tsx
@@ -1,74 +1,62 @@
-import { DateTime } from "luxon";
-import { useState } from "react";
-import ArrowComponent from "./components/ArrowComponent";
-import ComponentBox from "./components/ComponentBox";
-import NavBar from "./components/NavBar";
-import { getWeeksInMonth } from "./components/utils/dateUtils";
-import { DisplayDayDate } from "./entities/DisplayDate";
-import { MonthDate } from "./entities/MonthDate";
-import { WeekDate } from "./entities/WeekDate";
-import { YearDate } from "./entities/YearDate";
 import MapFull from "./components/MapFull";
 
 function App() {
-  const todayDate = DateTime.now();
+  // const todayDate = DateTime.now();
 
-  const startDay = todayDate.day;
-  const startMonth = todayDate.month;
-  const startYear = todayDate.year;
+  // const startDay = todayDate.day;
+  // const startMonth = todayDate.month;
+  // const startYear = todayDate.year;
 
-  const [displayDayDate, setDisplayDayDate] = useState<DisplayDayDate>({
-    day: startDay,
-    month: startMonth,
-    year: startYear,
-  });
+  // // const [displayDayDate, setDisplayDayDate] = useState<DisplayDayDate>({
+  // //   day: startDay,
+  // //   month: startMonth,
+  // //   year: startYear,
+  // // });
 
-  const startOfWeek = todayDate.startOf("week");
-  const endOfWeek = todayDate.endOf("week");
+  // // const startOfWeek = todayDate.startOf("week");
+  // // const endOfWeek = todayDate.endOf("week");
 
-  const formattedStartOfWeek = startOfWeek.toFormat("dd.MM.yyyy");
-  const formattedEndOfWeek = endOfWeek.toFormat("dd.MM.yyyy");
+  // // const formattedStartOfWeek = startOfWeek.toFormat("dd.MM.yyyy");
+  // // const formattedEndOfWeek = endOfWeek.toFormat("dd.MM.yyyy");
 
-  // Calculate timestamps for the next week
-  const startWeekTimeStamp = Math.floor(startOfWeek.toSeconds());
-  const endWeekTimeStamp = Math.floor(endOfWeek.toSeconds());
+  // // // Calculate timestamps for the next week
+  // // const startWeekTimeStamp = Math.floor(startOfWeek.toSeconds());
+  // // const endWeekTimeStamp = Math.floor(endOfWeek.toSeconds());
 
-  const [displayWeekDate, setDisplayWeekDate] = useState<WeekDate>({
-    start: formattedStartOfWeek,
-    end: formattedEndOfWeek,
-    startTimeStamp: startWeekTimeStamp,
-    endTimeStamp: endWeekTimeStamp,
-  });
+  // // // const [displayWeekDate, setDisplayWeekDate] = useState<WeekDate>({
+  // // //   start: formattedStartOfWeek,
+  // // //   end: formattedEndOfWeek,
+  // // //   startTimeStamp: startWeekTimeStamp,
+  // // //   endTimeStamp: endWeekTimeStamp,
+  // // // });
 
-  const currentMonth = todayDate.toFormat("MMMM");
-  const weeksInMonth = getWeeksInMonth(todayDate.year, todayDate.month);
+  // // // const currentMonth = todayDate.toFormat("MMMM");
+  // // // const weeksInMonth = getWeeksInMonth(todayDate.year, todayDate.month);
 
-  // Find the first and last weeks in weeksInMonth
-  const start = weeksInMonth[0].start;
-  const end = weeksInMonth[weeksInMonth.length - 1].end;
+  // // // // Find the first and last weeks in weeksInMonth
+  // // // const start = weeksInMonth[0].start;
+  // // // const end = weeksInMonth[weeksInMonth.length - 1].end;
 
-  const [displayMonthDate, setDisplayMonthDate] = useState<MonthDate>({
-    start: start,
-    end: end,
-    monthName: currentMonth,
-    month: startMonth,
-    year: startYear,
-    weeksInMonth: weeksInMonth,
-  });
+  // const [displayMonthDate, setDisplayMonthDate] = useState<MonthDate>({
+  //   start: start,
+  //   end: end,
+  //   monthName: currentMonth,
+  //   month: startMonth,
+  //   year: startYear,
+  //   weeksInMonth: weeksInMonth,
+  // });
 
-  // Calculate timestamps for year view
-  const startTimeStampYear = Math.floor(todayDate.startOf("year").toSeconds());
-  const endTimeStampYear = Math.floor(todayDate.endOf("year").toSeconds());
+  // // Calculate timestamps for year view
+  // const startTimeStampYear = Math.floor(todayDate.startOf("year").toSeconds());
+  // const endTimeStampYear = Math.floor(todayDate.endOf("year").toSeconds());
 
-  const [displayYearDate, setDisplayYearDate] = useState<YearDate>({
-    year: startYear,
-    startTimeStamp: startTimeStampYear,
-    endTimeStamp: endTimeStampYear,
-  });
+  // const [displayYearDate, setDisplayYearDate] = useState<YearDate>({
+  //   year: startYear,
+  //   startTimeStamp: startTimeStampYear,
+  //   endTimeStamp: endTimeStampYear,
+  // });
 
-  const [selectedTab, setSelectedTab] = useState("DAY");
-
-  const mapPolylines = [[0, 0]];
+  // const [selectedTab, setSelectedTab] = useState("DAY");
 
   return (
     <>
@@ -95,10 +83,7 @@ function App() {
         setDisplayWeekDate={setDisplayWeekDate}
         setDisplayMonthDate={setDisplayMonthDate}
       /> */}
-      <MapFull
-        coordinates={{ lat: 50.7902, lng: 15.075354999999998 }}
-        mapPolylines={mapPolylines}
-      />
+      <MapFull coordinates={{ lat: 50.7902, lng: 15.075354999999998 }} />
     </>
   );
 }
--- fitness-tracker/src/components/MapFull.tsx
+++ fitness-tracker/src/components/MapFull.tsx
@@ -1,8 +1,7 @@
 import { useEffect } from "react";
-import { Polyline, useMap } from "react-leaflet";
+import { useMap } from "react-leaflet";
 import { MapContainer } from "react-leaflet/MapContainer";
 
-import { LatLngExpression } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { TileLayer } from "react-leaflet/TileLayer";
 
@@ -13,10 +12,9 @@ export interface coordinatesType {
 
 interface Props {
   coordinates: coordinatesType;
-  mapPolylines: LatLngExpression[][];
 }
 
-const MapFull = ({ coordinates, mapPolylines }: Props) => {
+const MapFull = ({ coordinates }: Props) => {
   const MapUpdater = () => {
     const map = useMap();
     useEffect(() => {
@@ -44,7 +42,6 @@ const MapFull = ({ coordinates, mapPolylines }: Props) => {
           attribution='&copy; <a href="https://www.jawg.io/">Jawg</a>'
         />
 
-        <Polyline pathOptions={{ color: "blue" }} positions={mapPolylines} />
         <MapUpdater />
       </MapContainer>
     </div>
