b91b93b6e7d1ba4d9c69f715048130b2ed644c1f
Krystof Bumbalek <krystof.bumbalek@saboit.de>
2023-10-17 07:54:26 +0200

    index.ts added to the pages

--- fitness-tracker/src/App.tsx
+++ fitness-tracker/src/App.tsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import ArrowComponent from "./components/ArrowComponent";
 import ComponentBox from "./components/ComponentBox";
 import { DayProvider } from "./components/DayPage";
-import MonthProvider from "./components/MonthPage/MonthProvider";
 import NavBar from "./components/NavBar";
-import WeekProvider from "./components/WeekPage/WeekProvider";
-import YearProvider from "./components/YearPage/YearProvider";
 import SelectedTabProvider from "./components/SelectedTab/SelectedTabProvider";
+import { MonthProvider } from "./components/MonthPage";
+import { WeekProvider } from "./components/WeekPage";
+import { YearProvider } from "./components/YearPage";
 
 function App() {
   const [fullMap, setFullMap] = useState(false);
---
+++ fitness-tracker/src/components/WeekPage/index.ts
@@ -0,0 +1,1 @@
+export { default as WeekProvider } from "./WeekProvider";
